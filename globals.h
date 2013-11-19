477000caddd0a6dd11c601c41c4c1adf1eb79718
unknown <bbutsch@BBUTSCH-LAPTOP.TMS.local>
2013-11-18 21:57:20 -0500

    BMB commit initial files for final project.  Not much here yet

---
+++ globals.h
@@ -0,0 +1,11 @@
+// globals.h
+// add a better header here
+
+#define	carrier_size	5
+#define battleship_size	4
+#define submarine_size	3
+#define cruiser_size	3
+#define destroyer_size	2
+
+#define cursor_start_x	0	// x-axis coordinate of cursor starting position
+#define cursor_start_y	0	// y-axis coordinate of cursor starting position

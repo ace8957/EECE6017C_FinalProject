3139319f136ef4abbd3dd390bc93f9b79c7bd71c
Jordan Ross <ross.j101@gmail.com>
2013-11-24 15:50:03 -0500

    Got the serial port to output a hello world message using some code I found online

--- application/serial.c
+++ application/serial.c
@@ -9,19 +9,34 @@
 
 #include "serial.h"
 #include "globals.h"
+
+#define RS232_UART_DATA ((volatile int*) 0x10001010)
+#define RS232_UART_CONTROL ((volatile int*) (0x10001010+4))
  
 int sendSerialMessage(char msg)
 {
-    volatile int * RS232_data_ptr = (int *) 0x10001010; // RS232 data register address
-    volatile int * RS232_ctrl_ptr = (int *) 0x10001030; // RS232 control register address
+    //volatile int * RS232_data_ptr = (int *) 0x10001010; // RS232 data register address
+    //volatile int * RS232_ctrl_ptr = (int *) 0x10001030; // RS232 control register address
     
-    int data_mask = 0xFFFF;
-    int data = 0xFFFF;
+    //int data = 0xFFFF;
     
-    data = data_mask & (int)msg;
+    //data = *(RS232_data_ptr);  // read the Data register in the RS232 port
+    //data = (data & 0xFF00);    // extract the upper 2 bytes from the RS232 data register
+    //data  = data | msg;        // write the message to the lower 2 bytes of the data register    
+    //*(RS232_data_ptr) = data; // write to the data back into the data register
     
-    printf("data = %x",data);
-    
-    
+    unsigned char hwld[] = {'H','e','l','l','o',' ','W','o','r','l','d','\0'};
+    unsigned char *pOutput;
+
+    pOutput = hwld;
+    while(*pOutput) //strings in C are zero terminated
+    {
+         //if room in output buffer
+         if((*RS232_UART_CONTROL)&0xffff0000  ) 
+         {
+            //then write the next character
+            *RS232_UART_DATA = (*pOutput++); 
+         }
+     }
     return 1;
 }

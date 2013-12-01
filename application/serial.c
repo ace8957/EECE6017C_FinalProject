b9fd5423d26a63c042c916d7440d30bc6ba9b26d
Jordan Ross <ross.j101@gmail.com>
2013-12-01 12:35:56 -0500

    Some changes to test on the serial communication

--- application/serial.c
+++ application/serial.c
@@ -11,7 +11,7 @@
 #include "globals.h"
 
 #define RS232_UART_DATA ((volatile int*) 0x10001010)
-#define RS232_UART_READ_DATA ((volatile int*) 0x10001012)
+#define RS232_UART_READ_DATA ((volatile int*) 0x10001013)
 #define RS232_UART_CONTROL ((volatile int*) (0x10001014))
 
 /* API Function: sendGameBoard()
@@ -44,7 +44,7 @@ int* receiveGameBoard(void)
 } 
 
 // Helper functions
-
+/*
 int sendSerialMessage(unsigned char msg)
 {
     
@@ -72,40 +72,54 @@ int sendSerialMessage(unsigned char msg)
         return 0;
     }
 	return 1;
-}
+}*/
 
 int rx_Handshake(void) {
 	unsigned int data_reg;
 	unsigned int data_reg_old;
 	unsigned int control_reg;
+    unsigned int pe = 0;
 	while(1) {
 		data_reg_old = data_reg;
 		data_reg = ((*RS232_UART_READ_DATA) & 0x7F);
+        pe = ((*RS232_UART_DATA) & 0x100);
 		control_reg = (*RS232_UART_CONTROL);
 		// if(data_reg != 0 || control_reg != 0x800000)
 			// printf("Data reg: %x, Control reg: %x\n", data_reg, control_reg);
 		if(data_reg != data_reg_old)
-			printf("character: %c\n", (char)data_reg);
+        {
+            printf("RS232_UART_DATA: %x\n", (*RS232_UART_DATA));
+            printf("RS232_UART_READ_DATA: %x\n", (*RS232_UART_READ_DATA));
+			printf("character: %c\n", (char)data_reg);  
+            *RS232_UART_READ_DATA = (*RS232_UART_READ_DATA)*0x80;            
+        }
+        if (pe != 0)
+            printf("PE = 1\n");
 	}
 }
 
 int tx_Handshake(void)
 {
-    unsigned char tx_handshake[] = {'1', '1', '1', '1', '1', '1', '1', '1'};
+    unsigned char tx_handshake[] = {0x31,0, 0x31, 0, 0x31, 0, 0x31, 0};
     unsigned char rx_handshake[] = {'2', '2', '2', '2', '2', '2', '2', '2'};
     unsigned char *pOutput;
     
     unsigned int count = 0;
     
+    while(1)
+    {
     pOutput = tx_handshake;
     while(*pOutput) //strings in C are zero terminated
     {
          //if room in output buffer
          if((*RS232_UART_CONTROL)&0xffff0000  ) 
          {
+            printf("%x\n",*pOutput);
+            printf("%c\n",(char)(0xff & *pOutput));
             //then write the next character
             *RS232_UART_DATA = (*pOutput++); 
          }
+    }
     }
     while(1)
     {
